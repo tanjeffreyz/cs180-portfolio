54747af2ffd7fedea961926682e896b93fe6f03e
tanjeffreyz <tanjeffreyz02@berkeley.edu>
2023-09-02 00:02:16 -0700

    added p1

---
+++ p1-prokudin-gorskii/offsets.js
@@ -0,0 +1,134 @@
+const offsets = {
+  "emir.tif": {
+    "g_dy": 50,
+    "g_dx": 24,
+    "r_dy": 104,
+    "r_dx": 42
+  },
+  "_sunset_at_the_sea.tif": {
+    "g_dy": 45,
+    "g_dx": -26,
+    "r_dy": 115,
+    "r_dx": -56
+  },
+  "monastery.jpg": {
+    "g_dy": -3,
+    "g_dx": 2,
+    "r_dy": 3,
+    "r_dx": 2
+  },
+  "_conservatory.jpg": {
+    "g_dy": 6,
+    "g_dx": 3,
+    "r_dy": 13,
+    "r_dx": 4
+  },
+  "church.tif": {
+    "g_dy": 26,
+    "g_dx": 3,
+    "r_dy": 58,
+    "r_dx": -5
+  },
+  "three_generations.tif": {
+    "g_dy": 54,
+    "g_dx": 12,
+    "r_dy": 112,
+    "r_dx": 9
+  },
+  "melons.tif": {
+    "g_dy": 81,
+    "g_dx": 10,
+    "r_dy": 179,
+    "r_dx": 13
+  },
+  "_in_little_russia.tif": {
+    "g_dy": -23,
+    "g_dx": 7,
+    "r_dy": -34,
+    "r_dx": 10
+  },
+  "onion_church.tif": {
+    "g_dy": 51,
+    "g_dx": 26,
+    "r_dy": 108,
+    "r_dx": 36
+  },
+  "train.tif": {
+    "g_dy": 42,
+    "g_dx": 3,
+    "r_dy": 87,
+    "r_dx": 31
+  },
+  "tobolsk.jpg": {
+    "g_dy": 3,
+    "g_dx": 3,
+    "r_dy": 6,
+    "r_dx": 3
+  },
+  "icon.tif": {
+    "g_dy": 42,
+    "g_dx": 17,
+    "r_dy": 91,
+    "r_dx": 22
+  },
+  "_lilacs.tif": {
+    "g_dy": 51,
+    "g_dx": -7,
+    "r_dy": 99,
+    "r_dx": -26
+  },
+  "cathedral.jpg": {
+    "g_dy": 5,
+    "g_dx": 2,
+    "r_dy": 11,
+    "r_dx": 3
+  },
+  "_sunset.tif": {
+    "g_dy": 75,
+    "g_dx": -41,
+    "r_dy": 114,
+    "r_dx": -68
+  },
+  "_bird_cherry_tree.tif": {
+    "g_dy": -31,
+    "g_dx": -94,
+    "r_dy": 22,
+    "r_dx": -83
+  },
+  "_peonies.jpg": {
+    "g_dy": 8,
+    "g_dx": 2,
+    "r_dy": 15,
+    "r_dx": 3
+  },
+  "_urals.tif": {
+    "g_dy": 40,
+    "g_dx": 32,
+    "r_dy": 94,
+    "r_dx": 58
+  },
+  "self_portrait.tif": {
+    "g_dy": 77,
+    "g_dx": 27,
+    "r_dy": 176,
+    "r_dx": 36
+  },
+  "harvesters.tif": {
+    "g_dy": 60,
+    "g_dx": 17,
+    "r_dy": 125,
+    "r_dx": 15
+  },
+  "sculpture.tif": {
+    "g_dy": 33,
+    "g_dx": -11,
+    "r_dy": 140,
+    "r_dx": -26
+  },
+  "lady.tif": {
+    "g_dy": 56,
+    "g_dx": 8,
+    "r_dy": 104,
+    "r_dx": 2
+  }
+}
